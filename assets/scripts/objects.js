ffc52019372d1b048e25762671dc0b42696eaf54
Solomii <kushniryksolomiya@gmail.com>
2021-08-30 14:36:48 +0300

    studying objects

---
+++ assets/scripts/objects.js
@@ -0,0 +1,39 @@
+// const movieList = document.getElementById('movie-list');
+
+// movieList.style.backgroundColor = 'red';
+// movieList.style.display = 'block';
+
+// const userChosenKeyName = 'level';
+
+// let person = {
+//   'first name': 'Max',
+//   age: 30,
+//   hobbies: ['Sports', 'cooking'],
+//   // userChosenKeyName: '...',
+//    [userChosenKeyName]: '...',
+//   greet: function () {
+//     alert('Hi there!');
+//   },
+//   1.5: 'hello',
+// }
+
+
+// // person.age = 31;
+
+// // delete person.age;
+// // person.age = null;
+// person.isAdmin = true;
+
+// const keyName = 'first name';
+
+// // console.log(person['first name']);
+// console.log(person[keyName]);
+// console.log(person[1.5]);
+// console.log(person);
+
+const addMovieBtn = document.getElementById('add-movie-btn');
+const searchBtn = document.getElementById('search-btn');
+
+const addMovieHandler = () => {
+  const title = document.getElementById('title').value;
+}
